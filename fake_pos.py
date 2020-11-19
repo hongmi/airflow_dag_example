b79d1a5d4addfd53da873a37f962cb1dd49bfdf2
hongmin <lispmm@outlook.com>
2020-11-19 17:10:09 +0800

    a

--- fake_pos.py
+++ fake_pos.py
@@ -307,10 +307,10 @@ def task_func():
 
     now = datetime.datetime.now()
     dt = datetime.datetime.now()
-    while (now - dt).days < 2:
+    while (now - dt).days < 100:
         print(dt)
         log_timestamp('get orders')
-        orders = pos.get_orders(dt.date(), 1*1000)
+        orders = pos.get_orders(dt.date(), 1000*1000)
 
         log_timestamp('insert orders')
         mc['trans']['trans_1m_180day'].insert_many(orders)
